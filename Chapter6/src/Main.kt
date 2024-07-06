05c00e98c36a9fff6d6916a7c74fdc561763d2a8
박준기 <qmqqqqm@gmail.com>
2024-07-06 18:24:13 +0900

    Chapter6 6.3.1 인라인 클래스 정의하기 완료

--- Chapter6/src/Main.kt
+++ Chapter6/src/Main.kt
@@ -242,6 +242,24 @@ fun main() {
     이런 문제를 해결하기위해 코틀린에서는 인라인 클래스라는 새로운 종류의 클래스를 도입했다. 일반적인 원시 타입의 값과 마찬가지로 부가 비용 없이
     쓸 수 있기 때문에 인라인 클래스를 값 클래스라고 부르기도 한다.
     * */
+
+    /* 6.3.1 인라인 클래스 정의하기
+    인라인 클래스를 정의하려면 value class를 클래스 이름 앞에 붙여야하낟.
+    또한 JVM 백엔드를 사용하는 경우에는 @JvmInline을 value class 앞에 반드시 붙여줘야 한다.
+
+    @JvmInline
+    value class Dollar(val amount: Int) // amount의 단위는 센트
+
+    @JvmInline
+    value class Euro(val amount: Int) // amount의 단위는 센트
+
+    인라인 클래스의 주생성자에는 불변 프로퍼티를 하나만 선언해야 한다. 런타임에 클래스 인스턴스는 별도의 래퍼 객체를 생성하지 않고
+    이 프로퍼티의 값으로 표현된다. 그래서 인라인 클래스라는 이름이 붙었다. 런타임에는 호출 지점을 함수 본문으로 인라인하는 인라인 함수처럼
+    인라은 클래스 객체를 사용하는 위치 대신 인라인 클래스에 들었있는 값이 들어간다.
+    인라인 클래스도 자체 함수와 프로퍼티를 포함할 수 있다.
+    * */
+
+
 }
 
 enum class WeekDay {
@@ -264,3 +282,4 @@ enum class RainbowColor(val isCold: Boolean) {
 enum class Direction {
     NORTH, SOUTH, WEST, EAST;
 }
+
